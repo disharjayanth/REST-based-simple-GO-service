c1a2b3d41b491e18e6a99ac58e2126c753f85c56
disharjayantha <dishuj15@gmail.com>
2020-11-16 16:34:58 +0530

    simple go web REST service

---
+++ go.mod
@@ -0,0 +1,12 @@
+module Users/disharjayantha/go/src/chap10_deploy/webService
+
+go 1.15
+
+require (
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/kr/pretty v0.2.1 // indirect
+	github.com/lib/pq v1.8.0
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/tools/godep v0.0.0-20180126220526-ce0bfadeb516 // indirect
+	golang.org/x/tools v0.0.0-20201116002733-ac45abd4c88c // indirect
+)
---
+++ server.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"strconv"
+)
+
+// Post struch with fields ID, Content, Author
+type Post struct {
+	ID      int    `json:"id"`
+	Content string `json:"content"`
+	Author  string `json:"author"`
+}
+
+func main() {
+	server := http.Server{
+		Addr: ":" + os.Getenv("PORT"),
+	}
+	http.HandleFunc("/post/", handleRequest)
+	log.Println("Server Started Listening....")
+	server.ListenAndServe()
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	var err error
+	switch r.Method {
+	case "GET":
+		err = handleGet(w, r)
+	case "POST":
+		err = handlePost(w, r)
+	case "PUT":
+		err = handlePut(w, r)
+	case "DELETE":
+		err = handleDelete(w, r)
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request) (err error) {
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	if err != nil {
+		return
+	}
+	post, err := retrieve(id)
+	if err != nil {
+		return
+	}
+	jsonData, err := json.MarshalIndent(post, "", "\t")
+	if err != nil {
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(jsonData)
+	return
+}
+
+func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
+	len := r.ContentLength
+	body := make([]byte, len)
+	r.Body.Read(body)
+	var post Post
+	err = json.Unmarshal(body, &post)
+	if err != nil {
+		return
+	}
+	err = post.create()
+	if err != nil {
+		return
+	}
+	w.WriteHeader(200)
+	return
+}
+
+func handlePut(w http.ResponseWriter, r *http.Request) (err error) {
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	if err != nil {
+		return
+	}
+	post, err := retrieve(id)
+	if err != nil {
+		return
+	}
+	len := r.ContentLength
+	body := make([]byte, len)
+	r.Body.Read(body)
+	err = json.Unmarshal(body, &post)
+	if err != nil {
+		return
+	}
+	err = post.update()
+	if err != nil {
+		return
+	}
+	w.WriteHeader(200)
+	return
+}
+
+func handleDelete(w http.ResponseWriter, r *http.Request) (err error) {
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	if err != nil {
+		return
+	}
+	post, err := retrieve(id)
+	if err != nil {
+		return
+	}
+	err = post.delete()
+	if err != nil {
+		return
+	}
+	w.WriteHeader(200)
+	return
+}
